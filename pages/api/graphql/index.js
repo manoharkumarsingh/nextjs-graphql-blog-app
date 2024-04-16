1679b4ebb1e1b685725ca30a06fd881f29b9ad2d
Manohar Singh <manoharsingh@Manohars-MacBook-Pro.local>
2024-04-16 14:06:09 +0530

    typesscript fixes

--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -10,10 +10,10 @@ import { NextApiRequest } from "next";
 import jwt from "jsonwebtoken";
 import { NextRequest } from "next/server";
 
-mongoose.connect(process.env.MONGO_URI!, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-} as ConnectOptions);
+});
 
 mongoose.connection.on("connected", () => {
   console.log("connected to mongodb");
@@ -23,35 +23,16 @@ mongoose.connection.on("error", (err) => {
   console.log("error connecting", err);
 });
 
-interface MyContext {
-  userId?: string;
-}
-
-const context = ({ req }: { req: NextApiRequest }): MyContext => {
-  const { authorization } = req.headers;
-  if (authorization) {
-    const token = authorization.split(" ")[1]; // Extract the token from the Authorization header
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as {
-      userId: string;
-    };
-    const userId = decodedToken.userId;
-    return { userId }; // Pass userId in the context
-  }
-  return {}; // Return an empty object if there's no authorization header
-};
-
 const server = new ApolloServer({
   resolvers,
   typeDefs,
 });
 
-export default startServerAndCreateNextHandler<NextRequest, MyContext>(server, {
+export default startServerAndCreateNextHandler(server, {
   context: async (req) => {
     const { authorization } = req.headers;
     if (authorization) {
-      const { userId } = jwt.verify(authorization, process.env.JWT_SECRET!) as {
-        userId: string;
-      };
+      const { userId } = jwt.verify(authorization, process.env.JWT_SECRET);
       return { userId };
     }
     return {};
